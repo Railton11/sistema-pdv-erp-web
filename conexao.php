1afef642c0269daf11b781e0d4799eb5441606cc
Railton Nogueira <nicolaufeitosa2222@gmail.com>
2023-03-11 02:35:24 -0300

    tratando erro de conexão

--- conexao.php
+++ conexao.php
@@ -4,6 +4,11 @@ require_once("config.php");
 
 date_default_timezone_set("America/Sao_Paulo");
 
-$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+try{
+    $pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
+}catch (Exception $e){
+    echo "Erro ao conectar com banco de dados! <p>" .$e;
+}
+
 
 ?>
